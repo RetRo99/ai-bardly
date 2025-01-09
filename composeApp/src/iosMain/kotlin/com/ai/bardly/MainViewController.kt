d5adbd84f37c65a90032133d289fa409656cf1e2
Rok Retar <rok.retar@devrev.ai>
2025-01-09 13:21:32 +0100

    Initialize firebase and crashlytics on iOS

--- composeApp/src/iosMain/kotlin/com/ai/bardly/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/ai/bardly/MainViewController.kt
@@ -1,5 +1,13 @@
 package com.ai.bardly
 
 import androidx.compose.ui.window.ComposeUIViewController
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.crashlytics.crashlytics
+import dev.gitlive.firebase.initialize
 
 fun MainViewController() = ComposeUIViewController { App() }
+
+fun init() {
+    Firebase.initialize()
+    Firebase.crashlytics.setCrashlyticsCollectionEnabled(true)
+}
