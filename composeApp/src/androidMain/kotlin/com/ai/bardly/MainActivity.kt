798509d8a10f07797e8443d1c81f6e77aec2da4f
Rok Retar <rok.retar@devrev.ai>
2025-01-09 15:19:46 +0100

    Initialize firebase before setting content

--- composeApp/src/androidMain/kotlin/com/ai/bardly/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ai/bardly/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Firebase.initialize(this)
         setContent {
-            Firebase.initialize(this)
             LaunchedEffect(isSystemInDarkTheme()) {
                 enableEdgeToEdge()
             }
