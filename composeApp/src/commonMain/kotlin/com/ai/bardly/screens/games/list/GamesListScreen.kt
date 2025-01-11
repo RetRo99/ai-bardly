dfa058c34b874d20943700fb3e8daa3e965210d1
Rok Retar <rok.retar@devrev.ai>
2025-01-11 19:23:19 +0100

    Set game card height to the max height of the row.

--- composeApp/src/commonMain/kotlin/com/ai/bardly/screens/games/list/GamesListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/screens/games/list/GamesListScreen.kt
@@ -6,12 +6,16 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import app.cash.paging.PagingData
 import app.cash.paging.compose.collectAsLazyPagingItems
@@ -70,7 +74,10 @@ private fun SharedTransitionScope.GamesList(
     onGameClicked: (GameUiModel) -> Unit
 ) {
     val items = games.collectAsLazyPagingItems()
+    val state = rememberLazyGridState()
+    val density = LocalDensity.current
     LazyVerticalGrid(
+        state = state,
         columns = GridCells.Fixed(2), // Ensures 2 cards per row
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -83,7 +90,12 @@ private fun SharedTransitionScope.GamesList(
         ) { index ->
             val game = items[index]
             if (game != null) {
+                val row = state.layoutInfo.visibleItemsInfo.find { it.index == index }?.row
+                val itemsInRow = state.layoutInfo.visibleItemsInfo.filter { it.row == row }
+                val maxHeightInRow = itemsInRow.maxOfOrNull { it.size.height }
+                val maxHeightInRowDp = with(density) { maxHeightInRow?.toDp() } ?: Dp.Unspecified
                 GameCard(
+                    modifier = Modifier.height(maxHeightInRowDp),
                     game = game,
                     onGameClicked = onGameClicked,
                     animatedVisibilityScope = animatedVisibilityScope
