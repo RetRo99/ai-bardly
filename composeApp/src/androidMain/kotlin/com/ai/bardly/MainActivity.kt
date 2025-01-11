0537adf135a32b2aac19c5c318c3fa30bf7b3bbc
Rok Retar <rok.retar@devrev.ai>
2025-01-11 19:41:00 +0100

    Configure edge to edge styling with status and navigation bar colors

--- composeApp/src/androidMain/kotlin/com/ai/bardly/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ai/bardly/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ai.bardly
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -12,11 +14,17 @@ import dev.gitlive.firebase.initialize
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         Firebase.initialize(this)
         setContent {
             LaunchedEffect(isSystemInDarkTheme()) {
-                enableEdgeToEdge()
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.light(
+                        Color.GRAY, Color.GRAY
+                    ),
+                    navigationBarStyle = SystemBarStyle.light(
+                        Color.WHITE, Color.WHITE
+                    )
+                )
             }
             App()
         }
