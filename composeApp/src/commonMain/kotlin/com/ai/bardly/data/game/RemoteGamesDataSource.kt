50cab62a66d0d884baf2a066d751c65833f823de
Rok Retar <rok.retar@devrev.ai>
2025-01-12 07:45:42 +0100

    Refactor: Move RemoteGamesDataSource to its own file

---
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/data/game/RemoteGamesDataSource.kt
@@ -0,0 +1,39 @@
+package com.ai.bardly.data.game
+
+import androidx.paging.PagingData
+import app.cash.paging.PagingConfig
+import com.ai.bardly.data.game.model.GameApiModel
+import com.ai.bardly.data.game.model.GamesListApiResponse
+import com.ai.bardly.networking.NetworkClient
+import com.ai.bardly.paging.CustomPager
+import com.ai.bardly.paging.PagingResult
+import kotlinx.coroutines.flow.Flow
+
+class RemoteGamesDataSource(
+    private val networkClient: NetworkClient,
+) : GamesDataSource {
+
+    override suspend fun getGames(): Flow<PagingData<GameApiModel>> {
+        return CustomPager(
+            config = PagingConfig(pageSize = 20),
+            initialKey = 0,
+            getItems = { key, _ ->
+                val response = getGamesUrl(key)
+                val prevKey =
+                    if (response.currentPage == 0) null else (response.currentPage.dec())
+                val nextKey =
+                    if (response.currentPage == response.totalPages) null else (response.currentPage.inc())
+                PagingResult(response.games, prevKey, nextKey)
+            }
+        ).pagingData
+    }
+
+    private suspend fun getGamesUrl(page: Int): GamesListApiResponse {
+        return networkClient.get<GamesListApiResponse>(
+            path = "games",
+            queryBuilder = {
+                "page" to page
+            }
+        ).getOrThrow()
+    }
+}
