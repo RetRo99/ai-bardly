607bc7dce7cf25e049a677562aba305492250c50
Rok Retar <rok.retar@devrev.ai>
2025-01-18 20:44:03 +0100

    Refactor: Save message to local after get answer from remote

--- composeApp/src/commonMain/kotlin/com/ai/bardly/data/chat/ChatsDataRepository.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/data/chat/ChatsDataRepository.kt
@@ -4,16 +4,24 @@ import com.ai.bardly.data.chat.local.ChatsLocalDataSource
 import com.ai.bardly.data.chat.remote.ChatsRemoteDataSource
 import com.ai.bardly.domain.chats.ChatsRepository
 import com.ai.bardly.domain.chats.model.MessageDomainModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class ChatsDataRepository(
     private val remoteChatsDataSource: ChatsRemoteDataSource,
     private val localChatsDataSource: ChatsLocalDataSource,
 ) : ChatsRepository {
-    override suspend fun getAnswerFor(request: MessageDomainModel): Result<MessageDomainModel> {
-        localChatsDataSource.saveMessage(request)
+
+    override suspend fun getAnswerFor(
+        request: MessageDomainModel
+    ): Result<MessageDomainModel> = coroutineScope {
+        val saveRequestDeferred = async { localChatsDataSource.saveMessage(request) }
         val answerMessage = remoteChatsDataSource.getAnswer(request)
-        localChatsDataSource.saveMessage(answerMessage.getOrThrow())
-        return answerMessage
+        val saveAnswerDeferred =
+            async { localChatsDataSource.saveMessage(answerMessage.getOrThrow()) }
+        saveAnswerDeferred.await()
+        saveRequestDeferred.await()
+        answerMessage
     }
 
     override suspend fun getMessages(gameId: Int): Result<List<MessageDomainModel>> {
