ac599d40583fa2acddfabe02832a641b23ee5e1b
Rok Retar <rok.retar@devrev.ai>
2025-01-19 09:09:47 +0100

    Remove EmptyScreenContent and update ChatsListScreen content

--- composeApp/src/commonMain/kotlin/com/ai/bardly/screens/chats/list/ChatsListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/screens/chats/list/ChatsListScreen.kt
@@ -1,14 +1,17 @@
 package com.ai.bardly.screens.chats.list
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.ai.bardly.screens.EmptyScreenContent
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun ChatsListScreen(
 ) {
-    val viewModel = koinViewModel<ChatsViewModel>()
-    EmptyScreenContent(Modifier.fillMaxSize(), "CHATS")
+    koinViewModel<ChatsViewModel>()
+    Box(Modifier.fillMaxSize()) {
+        Text("CHATS")
+    }
 }
