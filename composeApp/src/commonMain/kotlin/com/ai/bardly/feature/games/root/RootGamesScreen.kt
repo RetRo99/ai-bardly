0651b46277ccd76a20daeb96ea01c2797df06276
Rok Retar <rok.retar@devrev.ai>
2025-02-15 10:13:52 +0100

    feat: Refactor RootGamesScreen to utilize RootChildStack for improved child navigation

--- composeApp/src/commonMain/kotlin/com/ai/bardly/feature/games/root/RootGamesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/feature/games/root/RootGamesScreen.kt
@@ -4,32 +4,14 @@ import androidx.compose.runtime.Composable
 import com.ai.bardly.feature.chats.ui.chat.ChatScreen
 import com.ai.bardly.feature.games.ui.details.GameDetailsScreen
 import com.ai.bardly.feature.games.ui.list.GamesListScreen
-import com.arkivanov.decompose.ExperimentalDecomposeApi
-import com.arkivanov.decompose.extensions.compose.experimental.stack.ChildStack
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.PredictiveBackParams
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.plus
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.scale
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation
-import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.androidPredictiveBackAnimatable
+import com.ai.bardly.feature.home.root.RootChildStack
 
-@OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun RootGamesScreen(
     component: RootGamesComponent,
 ) {
-    ChildStack(
-        component.childStack,
-        animation = stackAnimation(
-            animator = fade() + scale(),
-            predictiveBackParams = {
-                PredictiveBackParams(
-                    backHandler = component.backHandler,
-                    onBack = component::onBackClicked,
-                    animatable = ::androidPredictiveBackAnimatable,
-                )
-            }
-        )
+    RootChildStack(
+        component,
     ) { child ->
         when (val screen = child.instance) {
             is RootGamesComponent.GamesChild.GamesList -> GamesListScreen(screen.component)
