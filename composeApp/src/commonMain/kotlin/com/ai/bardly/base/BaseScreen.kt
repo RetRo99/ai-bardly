3e04f032f17f1b0b16ab1dc9a75d9409d9138537
Rok Retar <rok.retar@devrev.ai>
2025-02-15 10:28:44 +0100

    feat: Refactor BaseScreen to remove Koin dependencies and simplify ViewModel handling

--- composeApp/src/commonMain/kotlin/com/ai/bardly/base/BaseScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/base/BaseScreen.kt
@@ -7,40 +7,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
-import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.parameter.parametersOf
-
-@Composable
-inline fun <reified ViewModel : BaseViewModel<ScreenViewState, Intent>, ScreenViewState, Intent : ScreenIntent> BaseScreen(
-    noinline loadingContent: @Composable () -> Unit = { LoadingScreen() },
-    noinline errorContent: @Composable (BaseViewState.Error) -> Unit = { ErrorScreen(it) },
-    vararg parameters: Any = emptyArray(),
-    noinline content: @Composable (ScreenViewState, IntentDispatcher<Intent>) -> Unit
-) {
-    val viewModel: ViewModel = koinViewModel { parametersOf(*parameters) }
-    val viewState by viewModel.viewState.collectAsState()
-
-    LaunchedEffect(Unit) {
-        viewModel.onScreenDisplayed()
-    }
-    Box(Modifier.fillMaxSize()) {
-        when (val state = viewState) {
-            is BaseViewState.Success -> content(
-                state.data,
-                IntentDispatcher(viewModel::onScreenIntent)
-            )
-
-            is BaseViewState.Error -> errorContent(state)
-            is BaseViewState.Loading -> loadingContent()
-        }
-    }
-}
 
 @Composable
 inline fun <ScreenViewState, Intent : ScreenIntent> BaseScreen(
