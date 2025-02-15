08a2dcfdb9b88aa68ec1f37439848b10d4d9f1d9
Rok Retar <rok.retar@devrev.ai>
2025-02-15 10:44:42 +0100

    feat: Inject Analytics and ChatsRepository dependencies into DefaultMainComponent

--- composeApp/src/commonMain/kotlin/com/ai/bardly/navigation/root/main/DefaultMainComponent.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/navigation/root/main/DefaultMainComponent.kt
@@ -1,5 +1,7 @@
 package com.ai.bardly.navigation.root.main
 
+import com.ai.bardly.analytics.Analytics
+import com.ai.bardly.feature.chats.domain.ChatsRepository
 import com.ai.bardly.feature.chats.domain.GetRecentChatsUseCase
 import com.ai.bardly.feature.chats.ui.root.DefaultRootRecentComponent
 import com.ai.bardly.feature.games.domain.GamesRepository
@@ -10,14 +12,14 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 class DefaultMainComponent(
     componentContext: ComponentContext,
-) : MainComponent, ComponentContext by componentContext, KoinComponent {
-    val gamesRepository by inject<GamesRepository>()
-    val recentChatUseCase by inject<GetRecentChatsUseCase>()
+    private val gamesRepository: GamesRepository,
+    private val getRecentChatsUseCase: GetRecentChatsUseCase,
+    private val analytics: Analytics,
+    private val chatsRepository: ChatsRepository,
+) : MainComponent, ComponentContext by componentContext {
     private val navigation = StackNavigation<MainComponent.MainConfig>()
 
     override val childStack = childStack(
@@ -43,6 +45,8 @@ class DefaultMainComponent(
             DefaultRootGamesComponent(
                 componentContext,
                 gamesRepository,
+                chatRepository = chatsRepository,
+                analytics = analytics,
             )
         )
 
@@ -50,12 +54,17 @@ class DefaultMainComponent(
             DefaultRootHomeComponent(
                 componentContext,
                 gamesRepository,
+                chatsRepository = chatsRepository,
+                analytics = analytics,
             )
         )
 
         MainComponent.MainConfig.RecentChats -> MainComponent.MainChild.RecentChats(
             DefaultRootRecentComponent(
                 componentContext,
+                recentChatUseCase = getRecentChatsUseCase,
+                chatRepository = chatsRepository,
+                analytics = analytics,
             )
         )
     }
