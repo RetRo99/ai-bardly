d83561ebf491eec2e2ba753b19b4c3dc4bf40f54
Rok Retar <rok.retar@devrev.ai>
2025-02-15 10:13:22 +0100

    feat: Refactor RootRecentScreen to utilize RootChildStack for improved navigation handling

--- composeApp/src/commonMain/kotlin/com/ai/bardly/feature/chats/ui/root/RootRecentScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/feature/chats/ui/root/RootRecentScreen.kt
@@ -3,31 +3,14 @@ package com.ai.bardly.feature.chats.ui.root
 import androidx.compose.runtime.Composable
 import com.ai.bardly.feature.chats.ui.chat.ChatScreen
 import com.ai.bardly.feature.chats.ui.recent.RecentChatsScreen
-import com.arkivanov.decompose.ExperimentalDecomposeApi
-import com.arkivanov.decompose.extensions.compose.experimental.stack.ChildStack
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.PredictiveBackParams
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.plus
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.scale
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation
-import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.androidPredictiveBackAnimatable
+import com.ai.bardly.feature.home.root.RootChildStack
 
-@OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun RootRecentScreen(
     component: RootRecentComponent,
 ) {
-    ChildStack(
-        component.childStack,
-        animation = stackAnimation(
-            animator = fade() + scale(), predictiveBackParams = {
-                PredictiveBackParams(
-                    backHandler = component.backHandler,
-                    onBack = component::onBackClicked,
-                    animatable = ::androidPredictiveBackAnimatable,
-                )
-            }
-        )
+    RootChildStack(
+        component
     ) { child ->
         when (val screen = child.instance) {
             is RootRecentComponent.RootRecentChild.RecentChats -> RecentChatsScreen(screen.component)
