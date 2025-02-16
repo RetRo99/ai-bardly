c7feee16f89339eb3dff5ba35aee3b85a4b72f3c
Rok Retar <rok.retar@devrev.ai>
2025-02-16 12:09:13 +0100

    feat: Move MainScreen to main module

---
+++ composeApp/src/commonMain/kotlin/com/ai/bardly/feature/main/MainScreen.kt
@@ -0,0 +1,116 @@
+package com.ai.bardly.feature.main
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.dp
+import com.ai.bardly.feature.main.chats.root.RootRecentScreen
+import com.ai.bardly.feature.main.games.root.RootGamesScreen
+import com.ai.bardly.feature.main.home.root.RootHomeScreen
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.experimental.stack.ChildStack
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@OptIn(ExperimentalDecomposeApi::class)
+@Composable
+fun MainScreen(component: MainPresenter) {
+    Surface {
+        Scaffold(
+            bottomBar = {
+                AnimatedVisibility(
+                    true,
+                    enter = slideInVertically(initialOffsetY = { it }),
+                    exit = slideOutVertically(targetOffsetY = { it })
+                ) {
+                    val stack by component.childStack.subscribeAsState()
+                    BottomBar(
+                        currentlyActiveChild = stack.active.instance,
+                        onNavigationClick = { destination ->
+                            component.navigate(destination)
+                        }
+                    )
+                }
+            }
+        ) { innerPadding ->
+            ChildStack(
+                modifier = Modifier.padding(innerPadding),
+                stack = component.childStack,
+                animation = stackAnimation(fade()),
+            ) {
+                when (val child = it.instance) {
+                    is MainPresenter.MainChild.GameList -> RootGamesScreen(child.component)
+                    is MainPresenter.MainChild.Home -> RootHomeScreen(child.component)
+                    is MainPresenter.MainChild.RecentChats -> RootRecentScreen(child.component)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    currentlyActiveChild: MainPresenter.MainChild,
+    onNavigationClick: (MainPresenter.MainConfig) -> Unit
+) {
+    val outlineColor = MaterialTheme.colorScheme.outline
+    NavigationBar(
+        modifier = Modifier.drawBehind {
+            val strokeWidth = 2.dp.toPx()
+            drawLine(
+                color = outlineColor,
+                start = Offset(0f, 0f),
+                end = Offset(size.width, 0f),
+                strokeWidth = strokeWidth
+            )
+        },
+        tonalElevation = 8.dp,
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+        MainPresenter.rootItems.forEach { destination ->
+            val isSelected = when (currentlyActiveChild) {
+                is MainPresenter.MainChild.Home -> destination == MainPresenter.MainConfig.Home
+                is MainPresenter.MainChild.GameList -> destination == MainPresenter.MainConfig.GameList
+                is MainPresenter.MainChild.RecentChats -> destination == MainPresenter.MainConfig.RecentChats
+            }
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(destination.icon),
+                        contentDescription = null,
+                        modifier = Modifier.alpha(if (isSelected) 1f else 0.5f)
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(destination.title),
+                        modifier = Modifier.alpha(if (isSelected) 1f else 0.5f)
+                    )
+                },
+                selected = false, // Always false otherwise there is overlay
+                onClick = {
+                    if (!isSelected) {
+                        onNavigationClick(destination)
+                    }
+                }
+            )
+        }
+    }
+}
