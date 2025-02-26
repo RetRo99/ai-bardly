3778978f3e36709d7b9dfd1d7cb994a733cace63
Rok Retar <rok.retar@devrev.ai>
2025-02-26 13:41:52 +0100

    feat: Update `generateBearerToken` method to use `AppResult` type alias in authentication layer

--- feature/auth/data/src/commonMain/kotlin/com/retro99/auth/data/AuthDataRepository.kt
+++ feature/auth/data/src/commonMain/kotlin/com/retro99/auth/data/AuthDataRepository.kt
@@ -2,6 +2,7 @@ package com.retro99.auth.data
 
 import com.retro99.auth.data.remote.AuthRemoteDataSource
 import com.retro99.auth.domain.AuthRepository
+import com.retro99.base.result.AppResult
 import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
@@ -13,7 +14,7 @@ import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 class AuthDataRepository(
     private val remoteSource: AuthRemoteDataSource,
 ) : AuthRepository {
-    override suspend fun generateBearerToken(id: String): Result<String> {
+    override suspend fun generateBearerToken(id: String): AppResult<String> {
         return remoteSource.generateBearerToken(id)
     }
 }
--- feature/auth/domain/src/commonMain/kotlin/com/retro99/auth/domain/AuthRepository.kt
+++ feature/auth/domain/src/commonMain/kotlin/com/retro99/auth/domain/AuthRepository.kt
@@ -1,5 +1,7 @@
 package com.retro99.auth.domain
 
+import com.retro99.base.result.AppResult
+
 interface AuthRepository {
-    suspend fun generateBearerToken(id: String): Result<String>
+    suspend fun generateBearerToken(id: String): AppResult<String>
 }
