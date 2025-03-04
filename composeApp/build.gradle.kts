5e99d3085cba73f190a36515e79d9ceadea18af5
CircleCI <ci@gmail.com>
2025-03-04 09:17:17 +0000

    [skip ci] chore: Version bump from 0.0.4 (16) to 0.0.5 (17)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,8 +98,8 @@ android {
         applicationId = "com.ai.bardly"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 16
-        versionName = "0.0.4"
+        versionCode = 17
+        versionName = "0.0.5"
     }
     packaging {
         resources {
