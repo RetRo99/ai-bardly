a8522943f1351d3a8f2611dc46299185f4678596
Rok Retar <rok.retar@devrev.ai>
2025-04-21 10:37:45 +0200

    Update Shelfs UI model

--- feature/shelfs/ui/src/commonMain/kotlin/com/bardly/shelfs/ui/model/ShelfUiModel.kt
+++ feature/shelfs/ui/src/commonMain/kotlin/com/bardly/shelfs/ui/model/ShelfUiModel.kt
@@ -2,6 +2,8 @@ package com.bardly.shelfs.ui.model
 
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.bardly.games.ui.model.GameUiModel
+import com.bardly.games.ui.model.toUiModel
 import com.retro99.paging.domain.PagingItem
 import com.retro99.shelfs.domain.model.ShelfDomainModel
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +13,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ShelfUiModel(
     override val id: String,
+    val name: String,
+    val games: List<GameUiModel>,
 ) : PagingItem
 
 fun PagingData<ShelfDomainModel>.toUiModels() = map(ShelfDomainModel::toUiModel)
@@ -18,4 +22,6 @@ fun Flow<PagingData<ShelfDomainModel>>.toUiModels() = map { it.toUiModels() }
 fun List<ShelfDomainModel>.toUiModel() = map(ShelfDomainModel::toUiModel)
 fun ShelfDomainModel.toUiModel() = ShelfUiModel(
     id = id,
+    name = name,
+    games = games.toUiModel(),
 )
