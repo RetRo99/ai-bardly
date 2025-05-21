58fa6f0aa2aa78a89ca6e44044da90f532eda679
Rok Retar <retar.rok@gmail.com>
2025-05-21 15:37:10 +0200

    feat: Add shelfs to GameDetailsViewState

--- feature/games/ui/src/commonMain/kotlin/com/bardly/games/ui/details/GameDetailsViewState.kt
+++ feature/games/ui/src/commonMain/kotlin/com/bardly/games/ui/details/GameDetailsViewState.kt
@@ -5,4 +5,5 @@ import com.bardly.games.ui.model.GameUiModel
 data class GameDetailsViewState(
     val game: GameUiModel,
     val isFavorite: Boolean? = null,
+    val shelfs: List<ShelfInfo> = emptyList(),
 )
