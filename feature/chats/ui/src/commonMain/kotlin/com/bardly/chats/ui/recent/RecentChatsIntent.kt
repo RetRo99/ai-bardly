63a36bb70e5cf501692923d6f340fab10b42f1b2
Rok Retar <retar.rok@gmail.com>
2025-06-02 10:24:54 +0200

    Refactor: Change game id type from Int to String in ui model and view intents

--- feature/chats/ui/src/commonMain/kotlin/com/bardly/chats/ui/recent/RecentChatsIntent.kt
+++ feature/chats/ui/src/commonMain/kotlin/com/bardly/chats/ui/recent/RecentChatsIntent.kt
@@ -5,6 +5,6 @@ import com.retro99.base.ui.BaseScreenIntent
 sealed interface RecentChatsIntent : BaseScreenIntent {
     data class RecentChatClicked(
         val gameTitle: String,
-        val gameId: Int,
+        val gameId: String,
     ) : RecentChatsIntent
 }
